be1c2d07918910c5aa9ddb10f03604b3ec063c82
PechNik3 <129718611+PechNik3@users.noreply.github.com>
2025-02-20 17:43:34 +1000

    Delete test_bot.py

--- test_bot.py
+++
@@ -1,8 +0,0 @@
-from bot.handlers import validate_schedule_args
-
-
-def test_validate_schedule_args():
-    assert validate_schedule_args(["2025-02-20", "12:00", "Hello"]) == ((datetime(2025, 2, 20, 12, 0), "Hello"), None)
-    assert validate_schedule_args(["wrong", "format"]) == (None, "Использование: /schedule YYYY-MM-DD HH:MM сообщение")
-    assert validate_schedule_args(["2025-02-20", "12:00"]) == (
-    None, "Использование: /schedule YYYY-MM-DD HH:MM сообщение")
